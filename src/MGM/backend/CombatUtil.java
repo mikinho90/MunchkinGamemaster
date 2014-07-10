9229ce95fc6647906abef2fdb07c0e02faa7bb37
gitHen <hendrik.janiesch+github@gmail.com>
2014-07-10 22:03:11 +0200

    + auskommentierte kleine testmain

--- src/MGM/backend/CombatUtil.java
+++ src/MGM/backend/CombatUtil.java
@@ -16,4 +16,34 @@ public class CombatUtil {
 		Integer result = (player1.getPower()+player2.getPower())-monster.getPower();
 		return result;
 	}
+	/*
+	public static void main(String[] args){
+		Player p1 = new Player("hendrik");
+		Player p2 = new Player("mike");
+		Monster mon = new Monster();
+		System.out.println(CombatUtil.fight(mon, p1));
+		mon.powerUP();
+		System.out.println(CombatUtil.fight(mon, p1));
+		mon.powerUP10();
+		System.out.println(CombatUtil.fight(mon, p1));
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.boniUP();
+		p1.levelUP();
+		p1.levelUP();
+		p1.levelUP();
+		p1.levelUP();
+		p1.levelUP();
+		p1.levelUP();	
+		System.out.println(CombatUtil.fight(mon, p1));
+		System.out.println(CombatUtil.fight(mon, p1,p2));	
+	}
+	*/
 }
