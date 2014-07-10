e70671d0e6944078100f9a4664a64b084b4fb6c5
gitHen <hendrik.janiesch+github@gmail.com>
2014-07-10 18:35:50 +0200

    +mikes alte klasse in asd

--- src/MGM/frontend/asd.java
+++ src/MGM/frontend/asd.java
@@ -1,5 +1,60 @@
 package MGM.frontend;
 
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+
+import MGM.ViewMGM;
+
 public class asd {
 
+	/*
+	 *public class ViewMGM extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel playerPanel;
+	private JPanel monsterPanel;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ViewMGM frame = new ViewMGM();
+					frame.setVisible(true);
+					frame.setSize(1280, 720);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+	public ViewMGM() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		playerPanel = new JPanel();
+		playerPanel.setBounds(0, 0, 301, 255);
+		FlowLayout flowLayout = (FlowLayout) playerPanel.getLayout();
+		playerPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		monsterPanel = new JPanel();
+		monsterPanel.setBounds(304, 0, 123, 255);
+		monsterPanel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		contentPane.setLayout(null);
+		contentPane.add(playerPanel);
+		contentPane.add(monsterPanel);
+		
+		
+		
+	}
+
+}
+
+	 */
 }
